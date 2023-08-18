f2dec45361c7facc3ad059a3a6795337ee5973e1
Shane <shane.bergman@corteva.com>
2023-08-18 13:29:58 -0600

    add tailwind globlas, db singulton, and link to new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Next.js sasd',
 };
 
 export default function Page() {
-  return <h1>Hello, Home page!</h1>;
+  return (
+    <>
+      <h1>Hello, Home page!</h1>
+      <Link href="/new">NEW</Link>
+    </>
+  );
 }
